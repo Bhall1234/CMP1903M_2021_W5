bec0e456e02f728230642e35c5590d86a442adb0
Mark Doughty <mdoughty@lincoln.ac.uk>
2021-03-13 18:28:13 +0000

    Add project files.

---
+++ CMP1903M_2021_W5/Names.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMP1903M_2021_W5
+{
+    class Names
+    {
+        //class Names to hold your list of names and various methods
+    }
+}
---
+++ CMP1903M_2021_W5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CMP1903M_2021_W5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List called 'names' to hold a list of names
+            List<string> names = new List<string>();
+
+            //Add the names in the workshop task sheet to the list
+            //Use 'Add()' or a list initialiser (List<string> names = new List<string>(){"Olivia", "Oliver", "etc"};)
+
+            //Check to see if some names are in the list...?
+
+
+            //Create a simple search method to check if a name is in the list - don't use LINQ!
+        }
+    }
+}
